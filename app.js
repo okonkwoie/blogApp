528d478eb289fe7bbe58f45686c6e1109e54ca36
ijeoma <oijeoma11@gmail.com>
2023-01-16 00:06:53 +0100

    api security added

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const config = require('./config/config')
 const mongodbConnect = require('./db/mongodb')
 const userRouter = require('./routes/userRoutes')
 const blogRouter = require('./routes/blogRoutes')
+const helmet = require('helmet')
+const rateLimit = require('express-rate-limit')
 
 
 // const auth0Middleware = require('./auth/auth0')
@@ -21,6 +23,18 @@ app.use('/api/v1/blogs', blogRouter)
 
 // mongodb connection 
 mongodbConnect
+
+// security middleware
+app.use(helmet())
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+// Apply the rate limiting middleware to all requests
+app.use(limiter)
 
 // error handler middleware
 app.use((err, req, res, next) => {
